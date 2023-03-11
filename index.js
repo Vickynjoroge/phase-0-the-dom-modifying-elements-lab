9db7cbf18de7c423c57477feb8af86b04f04e480
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-03-12 01:53:13 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+
+  let newHeader = document.createElement('h1')
+  newHeader.id = "victory";
+  newHeader.innerHTML ="Your Text Here";
+  document.body.appendChild(newHeader);
+  newHeader.innerHTML = "New Text Here";
+  newHeader.textContent = '${victory} is the champion';;
